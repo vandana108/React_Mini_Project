d873ffda2638cd660fea74602cb6d23df956dd55
vandana108 <thevandanarai108@gmail.com>
2025-04-12 12:06:53 +0530

    Add conditional statement

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const secondBasket = document.querySelector('.basket-2 span')
 const leftArrow = document.querySelector('.left-arrow')
 const rightArrow = document.querySelector('.right-arrow')
 
-let totalApples = 80;
+let totalApples = 10;
 let secondBasketAppleCount = 0;
 let firstBasketAppleCount = totalApples-secondBasketAppleCount
 
@@ -12,9 +12,24 @@ firstBasket.innerText = firstBasketAppleCount
 secondBasket.innerText =secondBasketAppleCount
 
 rightArrow.addEventListener('click', () => {
+  if( firstBasketAppleCount>=0){
   firstBasketAppleCount-- 
   firstBasket.innerText = firstBasketAppleCount
-  
+  }
+
   secondBasketAppleCount++
   secondBasket.innerText = secondBasketAppleCount
+  
+})
+
+
+leftArrow.addEventListener('click', () => {
+  if( firstBasketAppleCount>0){
+  firstBasketAppleCount++ 
+  firstBasket.innerText = firstBasketAppleCount
+  }
+
+  secondBasketAppleCount--
+  secondBasket.innerText = secondBasketAppleCount
+  
 })
